0d4fb82ee457a7c4680a97497edebc8c94ec90fc
Roman Kulikov <kulikovroman746@gmail.com>
2022-08-09 12:57:43 +0300

    add tasks

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ window.printTable = (num) => {
 window.getFilteredArray = (arr) => {
     console.log(getFilteredArrayTs(arr).join(', '));
 };
+
+
+const staelBalls = (store, amountBallsBag) => {
+    const sumBalls = store.reduce((sum, cur) => sum + cur, 0);
+    const percentStore = store.map(item => item * 100 / sumBalls )
+
+    const result = store.map((item, index) => {
+        const countBalls = Math.floor(amountBallsBag * percentStore[index] / 100);
+        return item > countBalls ? countBalls : item;
+    })
+
+    console.log(result);
+}
+
+
+staelBalls([100, 300, 200], 120);
+staelBalls([10, 30, 20], 150);
