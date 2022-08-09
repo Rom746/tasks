27ea114958fb7c60142e48d77946686ea8f9ef2e
Roman Kulikov <kulikovroman746@gmail.com>
2022-08-09 18:44:01 +0300

    Push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import  getFilteredArrayTs  from "./getFilteredArray.js";
+import getFilteredArrayTs from "./getFilteredArray.js";
 import printTableTs from "./printTable.js";
+import { staelBalls } from "./staelBalls.js";
 
 const array = [41, 55, 61, 1, 8, 27, 37, 39];
 console.log('Result:', getFilteredArrayTs(array).join(', '));
@@ -16,18 +17,15 @@ window.getFilteredArray = (arr) => {
 };
 
 
-const staelBalls = (store, amountBallsBag) => {
-    const sumBalls = store.reduce((sum, cur) => sum + cur, 0);
-    const percentStore = store.map(item => item * 100 / sumBalls )
-
-    const resultBag = store.map((item, index) => {
-        const countBalls = Math.floor(amountBallsBag * percentStore[index] / 100);
-        return item > countBalls ? countBalls : item;
-    })
-
-    console.log(resultBag);
-}
-
-
 staelBalls([100, 300, 200], 120);
 staelBalls([10, 30, 20], 150);
+staelBalls([10, 30, 20], 10);
+staelBalls([1, 1, 1], 1);
+staelBalls([2, 1, 2], 2);
+staelBalls([1, 3, 1], 4);
+staelBalls([10, 0, 2], 4);
+
+
+window.getStaelBalls = (store, count) => {
+    staelBalls(store, count);
+};
---
+++ staelBalls.js
@@ -0,0 +1,37 @@
+export const staelBalls = (store, amountBallsBag) => {
+    const sumBalls = store.reduce((sum, cur) => sum + cur, 0);
+    const percentStore = store.map((item, index) => [Math.abs(item) * 100 / sumBalls, index]);
+    const sorted = percentStore.sort((a, b) => b[0] - a[0]);
+    let tmpSum = 0;
+
+    const resultBag = sorted.map(item => {
+        const [percent, index] = item;
+        const countBalls = Math.round(amountBallsBag * percent / 100);
+        let resultCount;
+
+        if (store[index] < countBalls) {
+            resultCount = store[index];
+        }
+
+        else if (countBalls + tmpSum > amountBallsBag) {
+            resultCount = amountBallsBag - tmpSum;
+        }
+
+        else if (countBalls == 0) {
+            resultCount = percent == 0 ? 0 : amountBallsBag - tmpSum;
+        }
+
+        else {
+            resultCount = countBalls;
+        }
+
+        tmpSum += resultCount;
+        return [resultCount, index];
+
+    }).sort((a, b) => a[1] - b[1]).map(item => item[0]);
+
+    console.log('');
+    console.log('Склад :', store);
+    console.log('Вместимомть мешка :', amountBallsBag);
+    console.log('Мешок :', resultBag);
+}
